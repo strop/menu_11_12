0e18b3c17d1ce6d62874913de217fccea283a95a
J <a@a.pl>
2011-11-29 22:31:12 +0100

    Przypadki brzegowe + tu nawias, tam nawias

--- zad1/binewt.m
+++ zad1/binewt.m
@@ -1,13 +1,18 @@
 function [approx, ok] = binewt(f, lewy, prawy, res)
-  approx = []
+  approx = [];
+  approxbin = [];
   try
     [approxbin, lastlen] = bisekcja(f, prawy, lewy, res);
+    disp(approxbin)
+    approx = approxbin;
+    if length(approxbin) ~= 1
+      approxnewt = newton(f, approxbin(end), lastlen*0.01);
+      approx = [approxbin; approxnewt];
+    end;
+    ok = 1;
   catch
     lasterror.message
     ok = 0;
     return
   end
-  approxnewt = newton(f, approxbin(end), lastlen*0.01);
-  approx = [approxbin; approxnewt];
-  ok = 1;
 end
--- zad1/bisekcja.m
+++ zad1/bisekcja.m
@@ -1,7 +1,7 @@
 function [x0, lastlen] = bisekcja(f,lewy,prawy,res)
   assert(res > 0, "Błąd: podano ujemne residuum");
-  x0 = [];
   mid = (lewy+prawy)/2;
+  x0 = [mid];
   while abs(f(mid)) > res,
     assert(sign(f(lewy)) ~= sign(f(prawy)), "Błąd w trakcie wykonywania metody bisekcji: funkcja ma taki sam znak na obu końcach przedziału");
     mid = (lewy+prawy)/2;
@@ -12,6 +12,6 @@ function [x0, lastlen] = bisekcja(f,lewy,prawy,res)
       prawy = mid;
     end;
     lastlen = prawy-lewy;
-    printf("%f, %f\n" ,lewy,prawy)
+%    printf("%f, %f\n" ,lewy,prawy)
   end;
 end;
